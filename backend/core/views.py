72475f1336d1b066a9de0d9f5bd7d4f00316b73a
Alex Peng <81698776+alexypeng@users.noreply.github.com>
2025-10-13 00:57:49 -0400

    update views

--- backend/core/views.py
+++ backend/core/views.py
@@ -77,3 +77,65 @@ class UserViewSet(viewsets.ModelViewSet):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    
+    def get_serializer_class(self):
+        if self.action == "create":
+            return GroupCreateSerializer
+        return GroupSerializer
+    
+    def get_queryset(self):
+        """Return groups that user is a member of"""
+        return self.queryset.filter(members=self.request.user)
+    
+    def perform_create(self, serializer):
+        """Create group and add creator as first member"""
+        group = serializer.save(created_by=self.request.user)
+        group.members.add(self.request.user)
+        
+    @action(detail=True, methods=['post'])
+    def join(self, request, pk=None):
+        """Join a group"""
+        group = self.get_object()
+        if request.user in group.members.all():
+            return Response(
+                {'error': 'Already a member of this group'},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        
+        group.members.add(request.user)
+        return Response(
+            {'message': 'Successfully joined group'},
+            status=status.HTTP_200_OK
+        )
+    
+    @action(detail=True, methods=['post'])
+    def leave(self, request, pk=None):
+        """Leave a group"""
+        group = self.get_object()
+        if request.user not in group.members.all():
+            return Response(
+                {'error': 'Not a member of this group'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        group.members.remove(request.user)
+        return Response(
+            {'message': 'Successfully left the group'},
+            status=status.HTTP_200_OK
+        )
+    
+    @action(detail=True, methods=['get'])
+    def members(self, request, pk=None):
+        """List all members of a group"""
+        group = self.get_object()
+        serializer = UserProfileSerializer(group.members.all(), many=True)
+        return Response(serializer.data)
+
+class AlarmViewSet(viewsets.ModelViewSet):
+    queryset = Alarm.objects.all()
+    serializer_class = AlarmSerializer
+    
+    def get_queryset(self):
+        
