91074488ae2357f02653dcdb35691541cdfcf827
Alex Peng <81698776+alexypeng@users.noreply.github.com>
2026-03-10 19:31:59 -0400

    ensure it does not reap an alarm that is being used in the api

--- api/alarms/management/commands/scheduler.py
+++ api/alarms/management/commands/scheduler.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
 
         with transaction.atomic():
             abandoned_events = (
-                AlarmEvent.objects.select_for_update()
+                AlarmEvent.objects.select_for_update(of=("self",))
                 .filter(status__in=[AlarmEvent.Status.RINGING, AlarmEvent.Status.SILENCED], created_at__lte=threshold)
                 .select_related("alarm__group", "user")
             )
@@ -38,21 +38,21 @@ class Command(BaseCommand):
                 self.notify_group(channel_layer, event.alarm.group.id, str(event.id), event.user.display_name)
 
             missed_alarms = (
-                Alarm.objects.select_for_update()
+                Alarm.objects.select_for_update(of=("self",))
                 .filter(is_active=True, next_trigger_utc__lte=threshold)
                 .select_related("group", "user")
             )
 
-            for alarm in missed_alarms:
-                event = AlarmEvent.objects.create(alarm=alarm, user=alarm.user, status=AlarmEvent.Status.EXPIRED)
+        for alarm in missed_alarms:
+            event = AlarmEvent.objects.create(alarm=alarm, user=alarm.user, status=AlarmEvent.Status.EXPIRED)
 
-                if alarm.is_one_time:
-                    alarm.is_active = False
+            if alarm.is_one_time:
+                alarm.is_active = False
 
-                alarm.save(update_fields=["is_active", "next_trigger_utc"])
+            alarm.save(update_fields=["is_active", "next_trigger_utc"])
 
-                print(f"[{now}] Caught dead phone for {alarm.user.display_name}")
-                self.notify_group(channel_layer, alarm.group.id, str(event.id), alarm.user.display_name)
+            print(f"[{now}] Caught dead phone for {alarm.user.display_name}")
+            self.notify_group(channel_layer, alarm.group.id, str(event.id), alarm.user.display_name)
 
     def notify_group(self, channel_layer, group_id, event_id, user_display_name):
         if channel_layer:
