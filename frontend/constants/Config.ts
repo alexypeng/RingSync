dd6c6e817e9ee4bce56728d1fbfc67f9bbcac2f7
Alex Peng <81698776+alexypeng@users.noreply.github.com>
2026-04-07 02:11:42 -0400

    add server url to config

--- frontend/constants/Config.ts
+++ frontend/constants/Config.ts
@@ -4,4 +4,4 @@ const LOCAL_IP = "10.0.0.137";
 
 export const API_URL = __DEV__
     ? `http://${LOCAL_IP}:8000`
-    : "https://your-production-url.supabase.co";
+    : "server-production-9114.up.railway.app";
